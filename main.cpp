0e9a898acfc6d6e4b99f3f490564485c0cfe16ae
nanino90 <ignacioutrilla@gmx.com>
2017-07-09 02:31:07 +0100

    Reduced time complexity to O(N)

--- main.cpp
+++ main.cpp
@@ -18,21 +18,50 @@ int solution(int A[], int N)
                         {
                         //profit
                                 if( (A[j]-A[i]) > profit)
+				{
                                         profit = (A[j]-A[i]);
-                        }
-                }
-        }
-        return profit;
+				} 
+			}
+		}
+	}
+	return profit;
+}
+
+int solution_2(int A[], int N)
+{
+	int profit =0;
+	int min	= A[0];
+
+	for(auto i = 1 ; i != N ; ++i )
+	{
+		if(A[i]>A[i-1])
+		{
+			//profit
+			if(profit< A[i]-min)
+			{	
+				profit= A[i]-min;
+			}
+		}
+		else
+		{
+			//new min
+			if(min>A[i])
+				min = A[i];
+		}
+	}
+	return profit;
 }
 
 int  main()
 {
-        int A[]={23171,21011,21123,21366,21013,21367};
+	int A[]={23171,21011,21123,21366,21013,21367};
 
-        int result = solution(&A[0], (int)(sizeof(A)/sizeof(*A)) );
+	int result = solution(&A[0], (int)(sizeof(A)/sizeof(*A)) );
+	std::cout<<std::to_string(result)<<std::endl;
+	
+	result = solution_2(&A[0], (int)(sizeof(A)/sizeof(*A)) );
+	std::cout<<std::to_string(result)<<std::endl;
 
-        std::cout<<std::to_string(result)<<std::endl;
-
-        return 0;
+	return 0;
 }
-          
+
